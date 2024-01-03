d748d4f11c67bd7065e37ddab90bc8ca93d06cbc
MUGHAL-XD <144633898+MUGHAL-XD@users.noreply.github.com>
2024-01-03 16:16:03 +0500

    Update DOD.py

--- DOD.py
+++ DOD.py
@@ -1,8 +1,8 @@
 import os,platform
 os.system('git pull')
-os.system('touch proxies.txt')
-DOD=platform.architecture()[0]
-if DOD=="32bit":
-    print('Sorry Bro Your Device 32bit \n \033[1;32m [√] \033[1;31mThis Tool 32Bits Device Not Supported')
-elif DOD=="64bit":
+
+dod=platform.architecture()[0]
+if dod=="32bit":
+    print('Sorry 32 Bit Not Saported...')
+elif dod=="64bit":
     __import__("xd")
